4c811ffcbc171b4e68c5e82e5610e5dc0b8435dc
joaopedrobordin <73653589+joaopedrobordin@users.noreply.github.com>
2021-07-05 22:06:22 -0300

    update-9

--- banco.sql
+++ banco.sql
@@ -1,5 +1,5 @@
 bd_mercado
 
-CREATE TABLE `bd_mercado`.`tb_produto` ( `nome` VARCHAR(50) NOT NULL ,
+CREATE TABLE `tb_produto` ( `nome` VARCHAR(50) NOT NULL ,
             `preco` DECIMAL NOT NULL ,
             `categoria` VARCHAR(50)) ENGINE = InnoDB;
